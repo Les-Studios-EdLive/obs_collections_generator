1d5eb6be7931888bf999cf605f63f9f3ece683b2
apomalyn <chretienxavier42@gmail.com>
2022-02-05 13:37:41 -0500

    Generation of the dart cli

---
+++ collections-archive-cli/bin/collections_archive_cli.dart
@@ -0,0 +1,5 @@
+import 'package:collections_archive_cli/collections_archive_cli.dart' as collections_archive_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${collections_archive_cli.calculate()}!');
+}
---
+++ collections-archive-cli/test/collections_archive_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:collections_archive_cli/collections_archive_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
