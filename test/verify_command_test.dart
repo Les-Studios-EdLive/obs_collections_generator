d16623646d1c587f60b2849be870d18429da0a6e
apomalyn <chretienxavier42@gmail.com>
2022-02-19 23:07:51 -0500

    ...

---
+++ test/verify_command_test.dart
@@ -0,0 +1,11 @@
+import 'package:args/command_runner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Tests arguments validation', () => {
+    test("Configuration file not given should fail", () async {
+      expect(true, true, reason: "Please write tests...");
+    })
+
+  });
+}
